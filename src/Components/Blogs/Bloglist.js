66df2b40ee278cb99debb63626ee80c18b5ef24c
Gurjant Singh <gurjantrandhawa74456@gmail.com>
2022-12-03 20:55:38 +0530

    Blog-App implemented

--- src/Components/Blogs/Bloglist.js
+++ src/Components/Blogs/Bloglist.js
@@ -6,8 +6,8 @@ import Blogs from "./Blogs";
 function BlogList({blogs}) {
     return <Grid container spacing={5}>
         {
-            blogs && blogs.map((blog, i) => {
-                return <Grid item md={4} sm={6} xs={12} key={i}>
+            blogs && blogs.map((blog, id) => {
+                return <Grid item md={4} sm={6} xs={12} key={id}>
                     <Blogs blog={blog}/>
                 </Grid>
             })
